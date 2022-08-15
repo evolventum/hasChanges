80ce298f89478c3168d8c32546eb7c94f3ddd393
Kyrylo Onyshchuk <onyshchuk.k.y@gmail.com>
2022-08-15 11:18:55 +0300

    Initial Commit

---
+++ hasChanges/hasChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  hasChangesApp.swift
+//  hasChanges
+//
+//  Created by Kyrylo Onyshchuk on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct hasChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
